d9f485b627c0f56c903ddb29aad3d6bd668de646
sochetanie <allagorik2901@gmail.com>
2017-05-26 10:13:38 -0400

    Done.

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,21 @@
+class SessionsController < ApplicationController
+  def new
+
+  end
+
+  def create
+    if params[:name].present?
+    session[:name] = params[:name]
+    redirect_to root_path
+    else
+        redirect_to sessions_new_path
+    end
+  end
+
+  def destroy
+    session.delete :name
+    redirect_to login_path
+  end
+
+
+end
